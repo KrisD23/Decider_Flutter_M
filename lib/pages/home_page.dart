7ed0715a6e35bc18241fb1487d22aaab256bd97c
Krishan Dwivedi <krishandwive9@gmail.com>
2023-10-11 18:46:01 +0530

    Added the basic drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,12 +3,28 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  static const String routeName = '/home';
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        elevation: 0,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: const Text('Home'),
+              onTap: () => Navigator.of(context).pop(),
+            ),
+            ListTile(
+              title: const Text('Settings'),
+              onTap: () => Navigator.of(context).pop(),
+            )
+          ],
+        ),
+      ),
+      body: const Center(
         child: Text('Home Page'),
       ),
     );
