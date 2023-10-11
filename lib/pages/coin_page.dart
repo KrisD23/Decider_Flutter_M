85a86b02b8e7934486fb710931bde2e78c4c3414
Krishan Dwivedi <krishandwive9@gmail.com>
2023-10-11 23:07:37 +0530

    coin page setup completed

--- lib/pages/coin_page.dart
+++ lib/pages/coin_page.dart
@@ -1,8 +1,22 @@
 import 'package:decider_app/util/nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class CoinPage extends StatelessWidget {
+class CoinPage extends StatefulWidget {
   const CoinPage({super.key});
+
+  @override
+  State<CoinPage> createState() => _CoinPageState();
+}
+
+class _CoinPageState extends State<CoinPage> {
+  int coinNumber = 2;
+  void flipCoin() {
+    setState(() {
+      coinNumber = Random().nextInt(2) + 1;
+      print(coinNumber);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +26,17 @@ class CoinPage extends StatelessWidget {
         elevation: 0,
       ),
       drawer: const NavBar(),
-      body: const Center(
-        child: Text('Coin page'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('lib/assets/images/Coin$coinNumber.png'),
+            TextButton(
+              onPressed: flipCoin,
+              child: const Text('Flip Coin'),
+            ),
+          ],
+        ),
       ),
     );
   }
